b4a37a10ef416e41c34f747bdc805339d069a08b
puspa <puspapoudel2000@gmail.com>
2019-12-03 09:50:20 +0545

    bullet add

--- JS-PROJECTS/ast4/js/car.js
+++ JS-PROJECTS/ast4/js/car.js
@@ -1,4 +1,4 @@
-function CarGame(startGameButton, gameContainer, highestScore, scoreBoard) {
+  function CarGame(startGameButton, gameContainer, highestScore, scoreBoard) {
   this.startGameButton = startGameButton;
   this.scoreBoard = scoreBoard;
   this.highestScore = highestScore;
@@ -7,36 +7,38 @@ function CarGame(startGameButton, gameContainer, highestScore, scoreBoard) {
   this.playGame = null;
   this.highest = window.localStorage.getItem('highest') || 0;
   this.frames = 0;
-  this.countTime = 2;
+  this.countTime = 4;
   this.car;
   this.canPlayGame = false;
   this.opponentPositionArray = [110, 260, 410];
   this.obstacleBackgroundArray = ['images/enemy.png', 'images/enemy2.png', 'images/enemy.png'];
   this.obstacleArray = [];
   this.playerCar = {
-    xPosition: 260,
-    yPosition: 530
+    xPosition: 260,//left
+    yPosition: 530 //top
   };
   this.obstacleCar = {
-    xPosition: Math.random() - 0.5,
+    //position of obstacle car
     yPosition: -1 * (Math.floor(Math.random() * 100))
   };
-  this.yPosition = [0, 0, 0];
+  this.yPosition = [-100, -100, -100];
   this.animFrame;
   //this.delay;
   var setUpGameContainer;
   var that = this;
-  const SPEED = 0.5;
+  
 
   this.startGameButton.onclick = function (e) {
     if (that.canPlayGame) {
       return;
     }
     that.canPlayGame = true;
+    that.highestScore.innerHTML = that.highest;
     setUpGameContainer = new SetUpGameContainer(that.gameContainer);
     setUpGameContainer.animateBackground();
     that.startGame();
   }
+  //creating  div the opponent car
 
   this.setupGameAssets = function () {
     var opponent = document.createElement('div');
@@ -44,7 +46,7 @@ function CarGame(startGameButton, gameContainer, highestScore, scoreBoard) {
     this.gameContainer.appendChild(opponent);
     return opponent;
   }
-
+//position of opponent car on left and top
   this.drawGameAssets = function (i) {
     var yPosition = 0;
     var obstacle = this.setupGameAssets();
@@ -53,19 +55,21 @@ function CarGame(startGameButton, gameContainer, highestScore, scoreBoard) {
     obstacle.style.top = yPosition + 'px';
     this.obstacleArray.push(obstacle);
   }
+  // if opponent pass the lane frame increases with 1 point 
 
   this.updateGameAssets = function (opponent, i) {
     if (this.yPosition[i] >= 640) {
       this.frames++;
       console.log(this.frames);
-      this.increaseDifficulty = true;
+      // this.increaseDifficulty = true;(After multiple of 10 whether to increase or decrease the speed)
       var random = -1 * (Math.floor(Math.random() * 1000));
-      if(random - this.yPosition[i+1] <= 100){
-       random = -1 * (Math.floor(Math.random() * 1000));
-      }
+      // if(random - this.yPosition[i+1] <= 100){
+      //  random = -1 * (Math.floor(Math.random() * 1000));
+      // }
       this.yPosition[i] = random;
       this.scoreBoard.innerHTML = this.frames;
     }
+    // increasing the speed of the car on the multiple of 10
     if (this.frames % 10 == 0 && this.increaseDifficulty) {
       this.countTime += 0.5;
       this.increaseDifficulty=false;
@@ -76,11 +80,16 @@ function CarGame(startGameButton, gameContainer, highestScore, scoreBoard) {
 
   this.animateGameAssets = function () {
     var random = Math.floor(Math.random() * 270) + 100;
+    //opponent car ending point in animframe
     this.animFrame = requestAnimationFrame(this.animateGameAssets.bind(this));
+
+    //delay the interval of opponent car
     if (this.animFrame % random == 0 && this.obstacleArray.length < 3 && Math.random() < 0.5) {
       console.log(this.frames % random);
       this.drawGameAssets(this.obstacleArray.length);
     }
+
+    
     for (var i = 0; i < this.obstacleArray.length; i++) {
       this.obstacleCar.xPosition = this.opponentPositionArray[i];
       this.updateGameAssets(this.obstacleArray[i], i);
@@ -115,35 +124,116 @@ function PlayGame(player, setUpGameContainer, carGame) {
   this.player = player;
   this.setUpGameContainer = setUpGameContainer;
   this.carGame = carGame;
+  //this.boostObject = null;
   this.isCrashed = false;
+  this.bulletObject = null;
+  this.bulletPosition = {
+    yPosition: 520
+  }
+  // this.boostPosition = {
+  //   xPosition: 0,
+  //   yPosition: 0
+  // }
+  
+  //this.boostYPos = [0,0,0];
+  this.bulletArray = [];
+  this.maximumAmmo = 20;
+  this.bulletCount = this.maximumAmmo;
+  this.aniFrame = null;
   var that = this;
   document.onkeydown = function (e) {
     if (!that.isCrashed) {
       switch (e.which) {
-        case 37:
+        case 65:
           that.carGame.playerCar.xPosition -= 150;
           break;
-        case 39:
+        case 68:
           that.carGame.playerCar.xPosition += 150;
           break;
+          case 32:  // space bar for firing bullet gun 
+          that.fireBullets();
+          break;
+
+          
       }
       that.isCrashed = that.movePlayer(that.carGame.playerCar.xPosition);
       if(that.isCrashed){
-        that.resetGame();
+        that.isCrashed = false;
+        if (that.carGame.playerCar.xPosition > 450) {
+          that.carGame.playerCar.xPosition -= 150;
+        }
+        else {
+          that.carGame.playerCar.xPosition += 150;
+        }
       }
     }
-    
+  }
+  
+  this.loadBullets = function(){
+  var bullet = document. createElement('div');
+  console.log(bullet);
+  bullet.setAttribute=('class', 'bullet');
+  bullet.style.top = this.bulletPosition.yPosition + 'px';
+  bullet.style.left = this.carGame.playerCar + 20 + 'px';
+  this.setUpGameContainer.gameContainer.appendChild(bullet);
+  return bullet;
+  }
+
+ 
+
+  this.fireBullets = function () {
+    if (this.bulletCount !== 0) {
+      this.bulletCount--;
+      this.bulletPosition.yPosition = 503;
+      var bulletObject = this.loadBullets();
+      this.bulletObject = bulletObject;
+      this.moveBullets();
+    }
+  }
+  this.updateBulletPosition = function () {
+    this.bulletPosition.yPosition -= 3;
+    this.bulletObject.style.top = this.bulletPosition.yPosition + 'px';
+  }
+  this.moveBullets = function () {
+    this.updateBulletPosition();
+    this.aniFrame=requestAnimationFrame(this.moveBullets.bind(this));
+    for (var i = 0; i < this.carGame.obstacleArray.length ; i++) {
+      if (this.carGame.playerCar.xPosition === this.carGame.opponentPositionArray[i]) {
+        if (this.checkBulletCollision(i, this.bulletPosition.yPosition)) {
+          this.carGame.obstacleArray[i].display='none';
+          this.carGame.yPosition[i] = -1*Math.floor(Math.random()*300+200);
+          this.bulletObject.remove();
+          cancelAnimationFrame(that.aniFrame);
+          break;
+        }
+      }
+    }
+  }
+  this.checkBulletCollision = function (i, y) {
+    if (y <= this.carGame.yPosition[i]+100 && y+27 > this.carGame.yPosition[i]) {
+      // console.log("collide");
+      return true;
+    }
   }
 
   this.resetGame = function () {
     cancelAnimationFrame(this.setUpGameContainer.animationFrame);
     cancelAnimationFrame(this.carGame.animFrame);
     alert("Game Over");
-    location.reload();
+    this.player.remove();
+    while (this.carGame.gameContainer.hasChildNodes()) {
+      this.carGame.gameContainer.removeChild(this.carGame.gameContainer.lastChild);
+    }
+    this.carGame.scoreBoard.innerHTML = '0';
+    this.carGame.highestScore.innerHTML = window.localStorage.getItem('highest');
+    init();
   }
 
+    // location.reload();
+  
+
   this.movePlayer = function (position) {
-    if (position < 82 || position > 450) {
+    if (position < 0 || position > 500) {
       return true;
     }
     this.player.style.left = position + 'px';
@@ -152,14 +242,15 @@ function PlayGame(player, setUpGameContainer, carGame) {
 }
 
 
+
 function SetUpGameContainer(gameContainer) {
   this.gameContainer = gameContainer;
   this.animationFrame;
   var position = 0;
 
   this.animateBackground = function () {
-    ++position;
-    this.gameContainer.style.backgroundPosition = '0 ' + position + 'px';
+    position += 2;
+    this.gameContainer.style.backgroundPosition = '0 ' + position + 'px';// background (x position 0 and y vary accordingly)
     this.animationFrame = requestAnimationFrame(this.animateBackground.bind(this));
   }
 
@@ -174,12 +265,12 @@ function SetUpGameContainer(gameContainer) {
 
 
 
-
-
-
+function init() {
 var startGameButton = document.getElementById('start-game');
 var gameContainer = document.getElementById('game-container');
 var scoreBoard = document.getElementById('score-board');
 var highestScore = document.getElementsByTagName('span')[0];
 new CarGame(startGameButton, gameContainer,highestScore,scoreBoard);
 
+}
+init();
